edc8086997844d3e86e64b8d41ba078252ad28c7
sammetasaicharan <saicharansammeta00613@gmail.com>
2026-05-10 16:38:59 +0530

    update api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://flavour-pos-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 15000
 });
